7bac7e489960b6ce9247cebcd4ceb555e5dfca01
Eruhan <erhanasik8@gmail.com>
2023-01-22 18:57:57 +0300

    made spotify connection.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,12 +2,12 @@ import TracksSection from "./components/TracksSection";
 import EmbedSection from "./components/EmbedSection";
 import {Grid} from "@mui/material";
 import axios from "axios";
+import {useEffect} from "react";
 
+const client_id = process.env.spotifyClientId;
+const client_secret = process.env.spotifyClientSecret;
+const auth_token = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const getAuth = async () => {
-    const client_id = process.env.spotifyClientId;
-    const client_secret = process.env.spotifyClientSecret;
-    const auth_token = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-    console.log(auth_token, client_id, client_secret);
     try {
         //make post request to SPOTIFY API for access token, sending relavent info
         const token_url = 'https://accounts.spotify.com/api/token';
@@ -20,20 +20,36 @@ const getAuth = async () => {
         })
         //return access token
         return response.data.access_token;
-        //console.log(response.data.access_token);
+
     } catch (error) {
         //on fail, log the error in console
         console.log(error);
     }
 }
 const getAlbum = async () => {
-    console.log(await getAuth());
+    const access_token = await getAuth();
+
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/albums/3pJ9WVk6gzk3mgNjecQf35', {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        })
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getAlbum().then(res => console.log(res));
 const HomePage = () => {
 
+    const albums = getAlbum();
 
+    useEffect(() => {
+        console.log(albums);
+    }, [albums]);
+    //TODO: added useEffect to get album data from spotify api
     return (
         <Grid container spacing={0} sx={{height: '100vh'}}>
             <Grid item xs={6}>
