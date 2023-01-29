c3a0e6c1800a88db165d0f1c0afdaeb696c981a8
erhanask <erhanasik8@gmail.com>
2023-01-29 15:54:22 +0300

    desing fixes.

--- src/components/home/components/TracksSection/TracksSection.js
+++ src/components/home/components/TracksSection/TracksSection.js
@@ -20,62 +20,67 @@ const TracksSection = () => {
                     color: '#333333',
                     marginBottom: '34px'
                 }}>
-                Discover My Favourite Playlists/Tracks
+                Discover Featured Playlists/Tracks
             </Typography>
-            <Box>
-                <Typography
-                    variant={'p'}
-                    sx={{
-                        fontSize: '18px',
-                        fontWeight: '600',
-                        lineHeight: '20px',
-                        color: '#333333',
-                        marginBottom: '20px'
-                    }}>
-                    Those Are Flying
-                </Typography>
-                <Grid container sx={{marginTop:'20px'}}>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/3pJ9WVk6gzk3mgNjecQf35'} />
+            <Box sx={{
+                height: 'calc( 100vh - 175px)',
+                overflowY: 'scroll'
+            }}>
+                <Box>
+                    <Typography
+                        variant={'p'}
+                        sx={{
+                            fontSize: '18px',
+                            fontWeight: '600',
+                            lineHeight: '20px',
+                            color: '#333333',
+                            marginBottom: '20px'
+                        }}>
+                        Those Are Flying
+                    </Typography>
+                    <Grid container sx={{marginTop:'20px'}}>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/3pJ9WVk6gzk3mgNjecQf35'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/playlists/4OIRpNAO3GtUiyIhEA7sAz'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/4IUeKh2mYOX2njthGA8STM'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/7nZjTEugHd3pBazK9mok3p'} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/playlists/4OIRpNAO3GtUiyIhEA7sAz'} />
+                </Box>
+                <Box sx={{marginTop:'30px'}}>
+                    <Typography
+                        variant={'p'}
+                        sx={{
+                            fontSize: '18px',
+                            fontWeight: '600',
+                            lineHeight: '20px',
+                            color: '#333333',
+                            marginBottom: '20px'
+                        }}>
+                        Those Are Running
+                    </Typography>
+                    <Grid container sx={{marginTop:'20px'}}>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/3pJ9WVk6gzk3mgNjecQf35'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/playlists/4OIRpNAO3GtUiyIhEA7sAz'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/4IUeKh2mYOX2njthGA8STM'} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <SongsCard url={'https://api.spotify.com/v1/albums/7nZjTEugHd3pBazK9mok3p'} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/4IUeKh2mYOX2njthGA8STM'} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/7nZjTEugHd3pBazK9mok3p'} />
-                    </Grid>
-                </Grid>
-            </Box>
-            <Box sx={{marginTop:'30px'}}>
-                <Typography
-                    variant={'p'}
-                    sx={{
-                        fontSize: '18px',
-                        fontWeight: '600',
-                        lineHeight: '20px',
-                        color: '#333333',
-                        marginBottom: '20px'
-                    }}>
-                    Those Are Running
-                </Typography>
-                <Grid container sx={{marginTop:'20px'}}>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/3pJ9WVk6gzk3mgNjecQf35'} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/playlists/4OIRpNAO3GtUiyIhEA7sAz'} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/4IUeKh2mYOX2njthGA8STM'} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <SongsCard url={'https://api.spotify.com/v1/albums/7nZjTEugHd3pBazK9mok3p'} />
-                    </Grid>
-                </Grid>
-                {/*TODO: I will list my lovely tracks in there.*/}
+                    {/*TODO: I will list my lovely tracks in there.*/}
+                </Box>
             </Box>
         </Box>
     );
