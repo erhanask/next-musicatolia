f8625f0bdc114a57e1346de4c733fc49f0e38977
erhanask <erhanasik8@gmail.com>
2023-02-05 13:53:23 +0300

    added favourites without nice design.

--- src/components/favourites/components/FavouritesSection/FavouritesSection.js
+++ src/components/favourites/components/FavouritesSection/FavouritesSection.js
@@ -1,32 +1,44 @@
-import {Grid} from "@mui/material";
+import {CircularProgress, Grid} from "@mui/material";
 import FavTrackCard from "./components";
+import {useEffect, useState} from "react";
+import {fetchHandler} from "../../../../helpers";
 
 
 const FavouritesSection = () => {
 
+    const [playlists, setPlaylists] = useState(null);
+    const listItems = [];
+    const getPlaylists = async () => {
+        return await fetchHandler("https://api.spotify.com/v1/users/x11yvx5zawwdoa7x0k7yl6oqj/playlists");
+    }
 
-    return (
-        <Grid container spacing={0} sx={{height: '100vh'}}>
-            <Grid item xs={2}>
-                <FavTrackCard/>
+
+    useEffect(() => {
+        getPlaylists().then(res => setPlaylists(res.items));
+    }, [])
+
+    console.log(playlists);
+
+    // Pushing list items into listItems array.
+    if (playlists !== null) {
+        playlists.forEach((tracks,i) => {
+            listItems.push(
+                <Grid key={i} item xs={3}>
+                    <FavTrackCard playlist={tracks}/>
+                </Grid>
+            );
+        })
+    }
+
+    // If playlists not null foreach works.
+    return playlists !== null ?
+        (
+            <Grid container spacing={0} sx={{height: '100vh'}}>
+                {listItems}
             </Grid>
-            <Grid item xs={2}>
-                <FavTrackCard/>
-            </Grid>
-            <Grid item xs={2}>
-                <FavTrackCard/>
-            </Grid>
-            <Grid item xs={2}>
-                <FavTrackCard/>
-            </Grid>
-            <Grid item xs={2}>
-                <FavTrackCard/>
-            </Grid>
-            <Grid item xs={2}>
-                <FavTrackCard/>
-            </Grid>
-        </Grid>
-    );
+        )
+        :
+        (<CircularProgress color={"inherit"} sx={{display: 'flex', marginInline: 'auto', marginBlock: '10px'}}/>);
 }
 
 
--- src/components/favourites/components/FavouritesSection/components/FavTrackCard.js
+++ src/components/favourites/components/FavouritesSection/components/FavTrackCard.js
@@ -1,9 +1,62 @@
+import Link from "next/link";
+import {Card, CardActionArea, CardContent, CardMedia, CircularProgress, Grid, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {fetchHandler} from "../../../../../helpers";
 
 
-const FavTrackCard = () => {
+const FavTrackCard = ({playlist}) => {
+
+    const [tracks, setTracks] = useState(null);
+    const listItems = [];
+    const getTracks = async () => {
+        return await fetchHandler(playlist.tracks.href + '?limit=20');
+    }
 
 
-    return "this is FavTrackCard";
+    useEffect(() => {
+        getTracks().then(res => setTracks(res.items));
+    }, [])
+
+    console.log(tracks);
+
+    // Pushing list items into listItems array.
+    if (tracks !== null) {
+        tracks.forEach(({track}, i) => {
+            listItems.push(
+                <Grid key={i} item xs={12}>
+                    <Link href={""}>
+                        <Card>
+                            <CardActionArea>
+                                <CardContent sx={{padding: '5px 0 0', marginBottom: '0'}}>
+                                    <Typography gutterBottom sx={{
+                                        fontWeight: '600',
+                                        fontSize: '16px',
+                                        lineHeight: '110.5%',
+                                        color: '#333333'
+                                    }} component="b">
+                                        {track?.name}
+                                    </Typography>
+                                    <Typography gutterBottom sx={{
+                                        fontWeight: '400',
+                                        fontSize: '12px',
+                                        lineHeight: '110.5%',
+                                        color: '#4F4F4F'
+                                    }} component="p">
+                                        {track.artists === undefined ? '-' : track?.artists[0]?.name}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Link>
+                </Grid>
+            );
+        })
+    }
+
+
+    console.log(tracks);
+
+    return tracks !== null ? listItems : (<CircularProgress color={"inherit"} sx={{display: 'flex', marginInline: 'auto', marginBlock: '10px'}}/>);
 }
 
 
