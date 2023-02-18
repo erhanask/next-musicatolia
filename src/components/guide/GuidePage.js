c918971902ae7314390e128747753b5fe23034df
Eruhan <erhanasik8@gmail.com>
2023-02-18 22:50:34 +0300

    added accordion inside guide page.

--- src/components/guide/GuidePage.js
+++ src/components/guide/GuidePage.js
@@ -1,10 +1,91 @@
+import {Accordion, AccordionDetails, AccordionSummary, Box, Typography} from "@mui/material";
+import {ExpandMoreSharp} from "@mui/icons-material";
+import {useState} from "react";
 
 
 const GuidePage = () => {
 
+    const [expanded, setExpanded] = useState(false);
 
+    const handleChange =
+        (panel) => (isExpanded) => {
+            setExpanded(isExpanded ? panel : false);
+        };
 
-    return 'guide page.';
+    return (<Box>
+        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreSharp/>}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+            >
+                <Typography sx={{width: '33%', flexShrink: 0}}>
+                    General settings
+                </Typography>
+                <Typography sx={{color: 'text.secondary'}}>I am an accordion</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                    Aliquam eget maximus est, id dignissim quam.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreSharp/>}
+                aria-controls="panel2bh-content"
+                id="panel2bh-header"
+            >
+                <Typography sx={{width: '33%', flexShrink: 0}}>Users</Typography>
+                <Typography sx={{color: 'text.secondary'}}>
+                    You are currently not an owner
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+                    varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+                    laoreet.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreSharp/>}
+                aria-controls="panel3bh-content"
+                id="panel3bh-header"
+            >
+                <Typography sx={{width: '33%', flexShrink: 0}}>
+                    Advanced settings
+                </Typography>
+                <Typography sx={{color: 'text.secondary'}}>
+                    Filtering has been entirely disabled for whole web server
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                    amet egestas eros, vitae egestas augue. Duis vel est augue.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreSharp/>}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+            >
+                <Typography sx={{width: '33%', flexShrink: 0}}>Personal data</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                    amet egestas eros, vitae egestas augue. Duis vel est augue.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    </Box>);
 }
 
 export default GuidePage;
