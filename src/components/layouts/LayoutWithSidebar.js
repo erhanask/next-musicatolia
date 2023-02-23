d7a5b12f8a37be4a24f5026443574896d8030863
Eruhan <erhanasik8@gmail.com>
2023-02-23 21:00:39 +0300

    css fixes

--- src/components/layouts/LayoutWithSidebar.js
+++ src/components/layouts/LayoutWithSidebar.js
@@ -1,17 +1,25 @@
 import Sidebar from "./components/Sidebar";
-import {Grid} from "@mui/material";
+import {Grid, useMediaQuery, useTheme} from "@mui/material";
 import styles from "../../../styles/Home.module.css";
 
 
 const LayoutWithSidebar = ({page}) => {
 
+    const isDownMd = useMediaQuery(useTheme().breakpoints.down("md"));
+
+
     return (
         <main className={styles.main}>
             <Grid container spacing={0} sx={{minHeight: '100vh'}}>
-                <Grid item xs={1}>
+                <Grid item md={1} sx={{
+                    position: isDownMd ? 'absolute':'initial',
+                    zIndex: 2,
+                    width: isDownMd ? '17%':'initial',
+                    height: '100vh'
+                }}>
                     <Sidebar/>
                 </Grid>
-                <Grid item xs={11}>
+                <Grid item md={11}>
                     {page}
                 </Grid>
             </Grid>
