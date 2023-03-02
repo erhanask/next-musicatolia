6dc47c4a6ee6ee237ca89b86c66293176c3904b2
Eruhan <erhanasik8@gmail.com>
2023-03-02 20:11:37 +0300

    made firebase auth informations hidden

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   env:{
     spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
     spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    nextPublicFirebaseApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    nextPublicFirebaseAuthDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    nextPublicFirebaseProjectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    nextPublicFirebaseStorageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    nextPublicFirebaseMessagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    nextPublicFirebaseAppId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    nextPublicFirebaseMeasurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   }
 }
 
